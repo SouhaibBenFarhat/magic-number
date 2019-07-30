3d6454ec0b04ca6a183309fbd3090cf49132bc97
ggdaltoso <ggdaltoso@gmail.com>
2019-07-30 17:14:45 -0300

    Added <GlobalStyle />

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GlobalStyle } from '@react95/core';
 import './styles/index.css';
 import App from './app/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById('root'),
+);
